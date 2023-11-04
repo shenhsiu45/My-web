06b2c619e222831a58c2bd26a61b4f60b25fc67a
shenhsiu45 <samuelchang0405@gmail.com>
2023-11-04 14:39:08 +0800

    88

--- name-tag-generator-project-main/src/NameTagList.js
+++ name-tag-generator-project-main/src/NameTagList.js
@@ -1,9 +1,8 @@
 import React from "react";
-import NameTag from "./NameTag.js"; // 匯入 NameTag 組件
+import NameTag from "./NameTag.js";
 import "./styles.css";
 
 const NameTagList = (props) => {
-  // 定義一個函式，用於渲染單個名牌
   const renderNameTag = (name, index) => (
     <NameTag
       name={name}
@@ -12,10 +11,7 @@ const NameTagList = (props) => {
       index={index}
     />
   );
-
-  // 透過 map 函式將每個名字轉換為 NameTag 組件
   const NameTagElements = props.names.map(renderNameTag);
-
   return <main>{NameTagElements}</main>;
 };
 
